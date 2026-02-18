d9fcf0230c82ab2a5b9ddd5fd7086c8f0612f34d
chianyungcode <cnytechcode@gmail.com>
2026-02-18 21:49:19 +0700

    feat: add defaults

--- modules/defaults.nix
+++ modules/defaults.nix
@@ -3,5 +3,74 @@
   __findFile,
   inputs,
   ...
-}: {
+}:
+{
+  den.default = {
+    nixos.system.stateVersion = "25.11";
+    homeManager.home.stateVersion = "25.11";
+    darwin.system.stateVersion = "6";
+
+    nixos = {
+      imports = [
+        inputs.nix-index-database.nixosModules.nix-index
+        inputs.disko.nixosModules.disko
+      ];
+
+      nixpkgs.config.allowUnfree = true;
+      programs.nix-index-database.comma.enable = true;
+      programs.nix-ld.enable = true;
+
+      home-manager = {
+        useGlobalPkgs = true;
+        useUserPackages = true;
+        backupFileExtension = "backup";
+      };
+
+      nix = {
+        settings = {
+          warn-dirty = false;
+          extra-system-features = [ "recursive-nix" ];
+          experimental-features = [
+            "nix-command"
+            "flakes"
+            "pipe-operators"
+            "recursive-nix"
+          ];
+          trusted-users = [
+            "root"
+            "@wheel"
+          ];
+          substituters = [
+            "https://cache.nixos.org/"
+            "https://nix-community.cachix.org"
+          ];
+          trusted-public-keys = [
+            "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+            "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          ];
+        };
+      };
+    };
+
+    homeManager = {
+      systemd.user.startServices = "sd-switch";
+    };
+
+  };
+
+  den.default.includes = [
+    # Enable home-manager on all hosts.
+    <den/home-manager>
+
+    # Automatically create the user on host.
+    <den/primary-user>
+
+    # Autoset hostname
+    (den.lib.take.exactly (
+      { OS, host }:
+      den.lib.take.unused OS {
+        nixos.networking.hostName = host.hostName;
+      }
+    ))
+  ];
 }
--- modules/system/nvidia.nix
+++ modules/system/nvidia.nix
@@ -1,16 +1,24 @@
 {
-  den.aspects.system._.nvidia.nixos = {
-    services.xserver.videoDrivers = ["nvidia"];
-    hardware.graphics.enable = true;
-    hardware.graphics.enable32Bit = true;
-    hardware = {
-      nvidia = {
-        open = true;
-        nvidiaSettings = true;
-        modesetting.enable = true;
-        powerManagement.enable = true;
-        powerManagement.finegrained = false;
+  den.aspects.system._.nvidia.nixos =
+    { lib, ... }:
+    {
+      nixpkgs.config.allowUnfreePredicate =
+        pkg:
+        builtins.elem (lib.getName pkg) [
+          "nvidia-x11"
+        ];
+
+      services.xserver.videoDrivers = [ "nvidia" ];
+      hardware.graphics.enable = true;
+      hardware.graphics.enable32Bit = true;
+      hardware = {
+        nvidia = {
+          open = true;
+          nvidiaSettings = true;
+          modesetting.enable = true;
+          powerManagement.enable = true;
+          powerManagement.finegrained = false;
+        };
       };
     };
-  };
 }
