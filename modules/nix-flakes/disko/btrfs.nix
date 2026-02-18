f0e9a26d555602a75bc8047069fbf5cd80c069b6
chianyungcode <cnytechcode@gmail.com>
2026-02-18 19:44:39 +0700

    feat: add disko configurations

---
+++ modules/nix-flakes/disko/btrfs.nix
@@ -0,0 +1,61 @@
+let
+  device = "/dev/sda";
+  mountOptions = [
+    "noatime"
+    "compress=zstd"
+    "ssd"
+    "discard=async"
+    "space_cache=v2"
+  ];
+in
+{
+  den.aspects.disko._.btrfs.nixos.disko = {
+    devices = {
+      disk = {
+        btrfs = {
+          type = "disk";
+          inherit device;
+          content = {
+            type = "gpt";
+            partitions = {
+              ESP = {
+                priority = 1;
+                name = "ESP";
+                start = "1M";
+                end = "500M";
+                type = "EF00";
+                content = {
+                  type = "filesystem";
+                  format = "vfat";
+                  mountpoint = "/boot";
+                  mountOptions = [ "umask=0077" ];
+                };
+              };
+              root = {
+                size = "100%";
+                content = {
+                  type = "btrfs";
+                  extraArgs = [ "-f" ];
+                  subvolumes = {
+                    "@" = {
+                      mountpoint = "/";
+                      inherit mountOptions;
+                    };
+                    "@home" = {
+                      mountpoint = "/home";
+                      inherit mountOptions;
+                    };
+                    "@nix" = {
+                      mountpoint = "/nix";
+                      inherit mountOptions;
+                    };
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
