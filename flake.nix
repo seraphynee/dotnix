8d272fddcaf05b36889bbd62bc01fc374d4d04bf
chianyungcode <cnytechcode@gmail.com>
2026-02-18 21:44:33 +0700

    feat: add input zen and catppuccin

--- flake.nix
+++ flake.nix
@@ -4,17 +4,6 @@
   outputs = inputs: inputs.flake-parts.lib.mkFlake {inherit inputs;} (inputs.import-tree ./modules);
 
   inputs = {
-    # Den
-    den.url = "github:vic/den";
-    flake-aspects.url = "github:vic/flake-aspects";
-    flake-file.url = "github:vic/flake-file";
-    flake-parts = {
-      inputs.nixpkgs-lib.follows = "nixpkgs-lib";
-      url = "github:hercules-ci/flake-parts";
-    };
-    import-tree.url = "github:vic/import-tree";
-    systems.url = "github:nix-systems/default";
-
     # Core
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     nixpkgs-lib.follows = "nixpkgs";
@@ -32,6 +21,26 @@
     disko = {
       url = "github:nix-community/disko/latest";
       inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # Den
+    den.url = "github:vic/den";
+    flake-aspects.url = "github:vic/flake-aspects";
+    flake-file.url = "github:vic/flake-file";
+    flake-parts = {
+      inputs.nixpkgs-lib.follows = "nixpkgs-lib";
+      url = "github:hercules-ci/flake-parts";
+    };
+    import-tree.url = "github:vic/import-tree";
+    systems.url = "github:nix-systems/default";
+
+    # Software
+    catppuccin.url = "github:catppuccin/nix";
+
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
     };
   };
 }
