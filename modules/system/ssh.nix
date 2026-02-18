aa9c5cd8a29fef20aa6d8d7d35713c7c6bb96354
chianyungcode <cnytechcode@gmail.com>
2026-02-18 21:23:59 +0700

    feat: add system modules

---
+++ modules/system/ssh.nix
@@ -0,0 +1,45 @@
+{
+  den.aspects.system._.ssh = {
+    nixos = { };
+    homeManager = {
+      programs.ssh = {
+        matchBlocks = {
+          ghcny = {
+            hostname = "github.com";
+            user = "git";
+            identityFile = "~/.ssh_keys/github-cnycode.pub";
+          };
+          ghspy = {
+            hostname = "github.com";
+            user = "git";
+            identityFile = "~/.ssh_keys/github-seraphynee.pub";
+          };
+        };
+      };
+    };
+  };
+
+  den.aspects.system._.sshd = {
+    nixos = {
+      services.sshd.enable = true;
+      services.openssh = {
+        enable = true;
+        settings = {
+          AllowAgentForwarding = "yes";
+          PasswordAuthentication = false;
+        };
+      };
+    };
+
+    provides = {
+      forward-ports.nixos = {
+        virtualisation.vmVariant.virtualisation.forwardPorts = [
+          {
+            host.port = 2222;
+            guest.port = 22;
+          }
+        ];
+      };
+    };
+  };
+}
