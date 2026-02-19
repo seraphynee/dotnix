bd2489bd1231dc075fc7cdb4b402fbe2d31a8de4
chianyungcode <cnytechcode@gmail.com>
2026-02-19 08:00:54 +0700

    feat: add new hosts

---
+++ modules/hosts/acerus.nix
@@ -0,0 +1,51 @@
+{
+  __findFile,
+  constants,
+  ...
+}:
+{
+  den.hosts.x86_64-linux.acerus.users = {
+    ${constants.user_chianyung} = { };
+  };
+
+  den.aspects.acerus = {
+    nixos =
+      { lib, ... }:
+      {
+        imports = [ ];
+
+        boot.initrd.availableKernelModules = [
+          "nvme"
+          "ahci"
+          "xhci_pci"
+          "usbhid"
+          "usb_storage"
+          "sd_mod"
+          "sr_mod"
+        ];
+
+        disko.devices.disk.btrfs.device = lib.mkForce "/dev/disk/by-id/nvme-eui.002538ba11b6cb55";
+      };
+
+    includes = [
+      <disko/btrfs>
+
+      <system/systemd-boot>
+      <system/locale>
+      <system/ssh>
+      <system/sshd>
+      <system/audio>
+      <system/fonts>
+      <system/networking>
+      <system/nvidia>
+      <system/xdg>
+
+      # <desktop/sddm>
+      # <desktop/kde>
+      <desktop/gnome>
+
+      <apps/ghostty>
+      # <apps/zen>
+    ];
+  };
+}
