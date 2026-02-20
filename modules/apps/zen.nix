2579b3ef38c0e82258fdc3f12adf9a3688bfd341
chianyungcode <cnytechcode@gmail.com>
2026-02-20 19:46:11 +0700

    feat: configure zen browser to allowing use 1password

--- modules/apps/zen.nix
+++ modules/apps/zen.nix
@@ -1,8 +1,20 @@
 {inputs, ...}: {
-  den.aspects.apps._.zen.homeManager = {
-    imports = [inputs.zen-browser.homeModules.default];
-    programs.zen-browser.enable = true;
-    programs.zen-browser.suppressXdgMigrationWarning = true;
-    home.sessionVariables.BROWSER = "zen";
+  den.aspects.apps._.zen = {
+    nixos = {
+      environment.etc = {
+        "1password/custom_allowed_browsers" = {
+          text = ''
+            zen
+          ''; # or just "zen" if you use unwrapped package
+          mode = "0755";
+        };
+      };
+    };
+    homeManager = {
+      imports = [inputs.zen-browser.homeModules.default];
+      programs.zen-browser.enable = true;
+      programs.zen-browser.suppressXdgMigrationWarning = true;
+      home.sessionVariables.BROWSER = "zen";
+    };
   };
 }
