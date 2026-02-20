f3f4e0f9e61fe9e8a03a3132b6dbe7b0d28010dd
chianyungcode <cnytechcode@gmail.com>
2026-02-21 03:01:57 +0700

    feat: added niri

---
+++ modules/desktop/niri.nix
@@ -0,0 +1,22 @@
+{__findFile, ...}: {
+  den.aspects.desktop._.niri = {
+    includes = [<desktop/sddm>];
+
+    nixos = {
+      services.xserver.enable = true;
+      services.xserver.xkb.layout = "us";
+
+      programs.niri.enable = true;
+      security.polkit.enable = true;
+    };
+
+    homeManager = {
+      programs.niri.settings = {
+        binds = {
+          "Mod+Z".action.spawn = "zen";
+          "Mod+G".action.spawn = "ghostty";
+        };
+      };
+    };
+  };
+}
