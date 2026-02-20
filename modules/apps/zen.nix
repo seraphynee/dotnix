2bb64ce7c47874f8631ae83d35713fd3c8951f10
chianyungcode <cnytechcode@gmail.com>
2026-02-21 00:04:04 +0700

    WIP

--- modules/apps/zen.nix
+++ modules/apps/zen.nix
@@ -1,4 +1,8 @@
-{inputs, ...}: {
+{
+  inputs,
+  pkgs,
+  ...
+}: {
   den.aspects.apps._.zen = {
     nixos = {
       environment.etc = {
@@ -14,7 +18,32 @@
       imports = [inputs.zen-browser.homeModules.default];
       programs.zen-browser.enable = true;
       programs.zen-browser.suppressXdgMigrationWarning = true;
+      programs.zen-browser.darwinDefaultsId = pkgs.lib.mkIf pkgs.stdenv.isDarwin "org.mozilla.firefox.plist";
       home.sessionVariables.BROWSER = "zen";
+      programs.zen-browser.profiles."*".search = {
+        force = true; # Needed for nix to overwrite search settings on rebuild
+        default = "ddg"; # Aliased to duckduckgo, see other aliases in the link above
+        engines = {
+          # My NixOS Option and package search shortcut
+          mynixos = {
+            name = "My NixOS";
+            urls = [
+              {
+                template = "https://mynixos.com/search?q={searchTerms}";
+                params = [
+                  {
+                    name = "query";
+                    value = "searchTerms";
+                  }
+                ];
+              }
+            ];
+
+            icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+            definedAliases = ["@nx"]; # Keep in mind that aliases defined here only work if they start with "@"
+          };
+        };
+      };
     };
   };
 }
