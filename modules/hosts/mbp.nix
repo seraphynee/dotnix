c3ac20224d43c7707dce203e3286085fca0d1d74
chianyungcode <cnytechcode@gmail.com>
2026-02-21 21:49:19 +0700

    WIP

--- modules/hosts/mbp.nix
+++ modules/hosts/mbp.nix
@@ -8,6 +8,7 @@
   den.aspects.mbp = {
     includes = [
       <system/settings>
+      <security/sops>
     ];
 
     # homeManager = {
---
+++ modules/security/sops.nix
@@ -0,0 +1,20 @@
+{
+  __findFile,
+  inputs,
+  ...
+}: {
+  den.aspects.security._.sops = {
+    darwin = {pkgs, ...}: {
+      imports = [inputs.sops-nix.darwinModules.sops];
+
+      environment.systemPackages = with pkgs; [
+        sops
+        age
+        age-plugin-yubikey
+        yubikey-manager # optional but handy (ykman)
+        yubico-piv-tool # optional but handy (PIV debugging)
+        pcsclite
+      ];
+    };
+  };
+}
