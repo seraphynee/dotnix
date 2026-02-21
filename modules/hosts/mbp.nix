5a1d536a12bf3a5b8eb54802e8269f57977af5a3
chianyungcode <cnytechcode@gmail.com>
2026-02-21 16:38:31 +0700

    feat: delete zen config in dots folder

--- modules/hosts/mbp.nix
+++ modules/hosts/mbp.nix
@@ -2,33 +2,31 @@
   __findFile,
   constants,
   ...
-}: let
-  zenProfilePath = ../../dots/config/zen/Profiles/default;
-in {
+}: {
   den.hosts.aarch64-darwin.mbp.users.${constants.user_one} = {};
 
   den.aspects.mbp = {
     includes = [
     ];
 
-    homeManager = {
-      home.file."Library/Application Support/zen" = {
-        force = true;
-        recursive = true;
-        source = ../../dots/config/zen;
-      };
-
-      # home.file."Library/Application Support/Zen/profiles.ini".text = ''
-      #   [General]
-      #   StartWithLastProfile=1
-      #
-      #   [Profile0]
-      #   Default=1
-      #   IsRelative=1
-      #   Name=default
-      #   Path=Profiles/default
-      # '';
-    };
+    # homeManager = {
+    #   home.file."Library/Application Support/zen" = {
+    #     force = true;
+    #     recursive = true;
+    #     source = ../../dots/config/zen;
+    #   };
+    #
+    #   # home.file."Library/Application Support/Zen/profiles.ini".text = ''
+    #   #   [General]
+    #   #   StartWithLastProfile=1
+    #   #
+    #   #   [Profile0]
+    #   #   Default=1
+    #   #   IsRelative=1
+    #   #   Name=default
+    #   #   Path=Profiles/default
+    #   # '';
+    # };
 
     darwin = {
       system.defaults = {
