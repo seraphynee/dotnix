05383105dd9c9388d0dc53d64c004d1a46acceb1
chianyungcode <cnytechcode@gmail.com>
2026-02-21 14:58:11 +0700

    WIP: new defaults

--- modules/hosts/mbp.nix
+++ modules/hosts/mbp.nix
@@ -7,8 +7,26 @@
 
   den.aspects.mbp = {
     includes = [
-      <apps/zen>
     ];
+
+    homeManager = {
+      home.file."Library/Application Support/Zen/Profiles/default" = {
+        force = true;
+        recursive = true;
+        source = ../../dots/config/zen/Profiles/default;
+      };
+
+      home.file."Library/Application Support/Zen/profiles.ini".text = ''
+        [General]
+        StartWithLastProfile=1
+
+        [Profile0]
+        Default=1
+        IsRelative=1
+        Name=default
+        Path=Profiles/default
+      '';
+    };
 
     darwin = {
       system.defaults = {
