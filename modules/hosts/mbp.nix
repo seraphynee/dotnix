b5104af4f93241a1de8c4262307ef897f1c7baca
chianyungcode <cnytechcode@gmail.com>
2026-02-21 15:10:01 +0700

    update config zen for mac

--- modules/hosts/mbp.nix
+++ modules/hosts/mbp.nix
@@ -2,7 +2,9 @@
   __findFile,
   constants,
   ...
-}: {
+}: let
+  zenProfilePath = ../../dots/config/zen/Profiles/default;
+in {
   den.hosts.aarch64-darwin.mbp.users.${constants.user_one} = {};
 
   den.aspects.mbp = {
@@ -10,22 +12,22 @@
     ];
 
     homeManager = {
-      home.file."Library/Application Support/Zen/Profiles/default" = {
+      home.file."Library/Application Support/zen" = {
         force = true;
         recursive = true;
-        source = ../../dots/config/zen/Profiles/default;
+        source = ../../dots/config/zen;
       };
 
-      home.file."Library/Application Support/Zen/profiles.ini".text = ''
-        [General]
-        StartWithLastProfile=1
-
-        [Profile0]
-        Default=1
-        IsRelative=1
-        Name=default
-        Path=Profiles/default
-      '';
+      # home.file."Library/Application Support/Zen/profiles.ini".text = ''
+      #   [General]
+      #   StartWithLastProfile=1
+      #
+      #   [Profile0]
+      #   Default=1
+      #   IsRelative=1
+      #   Name=default
+      #   Path=Profiles/default
+      # '';
     };
 
     darwin = {
