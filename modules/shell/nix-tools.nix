122e24fdcee9d3526d9b9a6b9fd2494e11033627
chianyungcode <cnytechcode@gmail.com>
2026-02-26 22:02:23 +0700

    feat: add description to nix-tools

--- modules/shell/nix-tools.nix
+++ modules/shell/nix-tools.nix
@@ -5,16 +5,16 @@
       {
         home.packages = with pkgs; [
           # Nix
-          deadnix
-          nix-init
-          nix-inspect
-          nix-output-monitor
-          nixpkgs-review
-          nix-tree
-          nix-update
-          nurl
-          statix
-          vulnix
+          deadnix # Detect unused Nix code
+          nix-init # Generate a Nix package file from source projects
+          nix-inspect # Inspect derivations and Nix metadata
+          nix-output-monitor # Show cleaner Nix build progress output
+          nixpkgs-review # Build/test packages affected by nixpkgs changes
+          nix-tree # View the dependency tree of Nix packages
+          nix-update # Automatically update Nix package versions/sources
+          nurl # Generate fetcher expressions from source URLs
+          statix # Linter for Nix best practices
+          vulnix # Scan Nix dependencies for known vulnerabilities
         ];
       };
   };
