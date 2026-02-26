ff3707708a60bbd94a95bb3e46c7cc0cfbb119fd
chianyungcode <cnytechcode@gmail.com>
2026-02-26 21:31:54 +0700

    feat: update polkitPolicyOwners to using constants

--- modules/shell/1password.nix
+++ modules/shell/1password.nix
@@ -1,3 +1,4 @@
+{ constants, ... }:
 {
   den.aspects.shell._._1password = {
     nixos = {
@@ -6,7 +7,7 @@
         enable = true;
         # Certain features, including CLI integration and system authentication support,
         # require enabling PolKit integration on some desktop environments (e.g. Plasma).
-        polkitPolicyOwners = [ "chianyung" ];
+        polkitPolicyOwners = [ "${constants.user_one}" ];
       };
     };
     homeManager =
