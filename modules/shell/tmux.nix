7d2e82573b71790d2f69a076e42113f55c8c1e50
seraphynee <seraphyne31@gmail.com>
2026-03-06 22:05:57 +0700

    feat(tmux): add tmux aliases in fish shell

--- modules/shell/tmux.nix
+++ modules/shell/tmux.nix
@@ -1,6 +1,16 @@
+{ lib, ... }:
 {
   den.aspects.shell._.tmux.homeManager = {
-    programs.tmux.enable = true;
+    programs = {
+      fish.interactiveShellInit = lib.mkAfter ''
+        abbr --add tx tmux
+        abbr --add tl "tmux list-sessions"
+        abbr --add ts --set-cursor 'tmux new -s "%"'
+        abbr --add tksv "tmux kill-server"
+      '';
+
+      tmux.enable = true;
+    };
     xdg.configFile."tmux/tmux.conf".source = ../../dots/config/tmux/tmux.conf;
   };
 }
