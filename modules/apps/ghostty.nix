9cb68d275ab34c670d9512c7d20c854e08583cae
seraphynee <seraphyne31@gmail.com>
2026-03-06 22:17:35 +0700

    feat: add ghostty config

--- modules/apps/ghostty.nix
+++ modules/apps/ghostty.nix
@@ -1,34 +1,76 @@
 {
-  den.aspects.apps._.ghostty.homeManager = {
-    programs.ghostty = {
-      enable = true;
-      enableBashIntegration = true;
-      enableFishIntegration = false;
-      enableZshIntegration = true;
-      settings = {
-        font-family = "JetBrains Mono Nerd Font";
-        font-size = 14;
-        background-opacity = 0.9;
-        theme = "Catppuccin Mocha";
-        shell-integration-features = true;
-        window-decoration = "server";
-        window-padding-x = 4;
-        window-padding-color = "extend";
-        window-padding-balance = true;
-        keybind = [
-          "ctrl+alt+v=new_split:right"
-          "ctrl+alt+s=new_split:down"
-          "ctrl+alt+n=new_split:auto"
-          "ctrl+shift+h=goto_split:left"
-          "ctrl+shift+j=goto_split:bottom"
-          "ctrl+shift+k=goto_split:top"
-          "ctrl+shift+l=goto_split:right"
-          "ctrl+shift+w=close_surface"
-          "page_up=scroll_page_fractional:-0.5"
-          "page_down=scroll_page_fractional:+0.5"
-          "ctrl+shift+r=reload_config"
-        ];
+  den.aspects.apps._.ghostty.homeManager =
+    { pkgs, ... }:
+    {
+      xdg.configFile."ghostty" = {
+        source = ../../dots/config/ghostty;
+        recursive = true;
+
+      };
+
+      programs.ghostty = {
+        enable = true;
+        enableBashIntegration = true;
+        enableFishIntegration = false;
+        enableZshIntegration = true;
+        settings = {
+          shell-integration = "detect";
+          shell-integration-features = "cursor,sudo,title";
+
+          background-opacity = 0.8;
+          background-blur-radius = 10;
+          theme = "oldworld-vibrant";
+
+          cursor-color = "#00FF98";
+          cursor-style = "bar";
+          cursor-style-blink = false;
+
+          window-padding-x = 11;
+          window-padding-y = 11;
+          window-title-font-family = "Cascadia Code";
+
+          font-family = if pkgs.stdenv.hostPlatform.isDarwin then "Jetbrains Mono" else "CommitMono";
+          font-size = if pkgs.stdenv.hostPlatform.isDarwin then 19 else 13;
+          font-thicken = false;
+          font-thicken-strength = 150;
+
+          keybind = [
+            "alt+shift+t=toggle_quick_terminal"
+            "ctrl+shift+h=new_split:left"
+            "ctrl+shift+j=new_split:down"
+            "ctrl+shift+k=new_split:up"
+            "ctrl+shift+l=new_split:right"
+            "alt+shift+h=goto_split:left"
+            "alt+shift+j=goto_split:down"
+            "alt+shift+k=goto_split:up"
+            "alt+shift+l=goto_split:right"
+          ]
+          ++ pkgs.lib.optionals pkgs.stdenv.hostPlatform.isLinux [
+            "alt+one=unbind"
+            "alt+two=unbind"
+            "alt+three=unbind"
+            "alt+four=unbind"
+            "alt+five=unbind"
+            "alt+six=unbind"
+            "alt+seven=unbind"
+            "alt+eight=unbind"
+            "alt+nine=unbind"
+          ];
+
+          mouse-hide-while-typing = true;
+          copy-on-select = "clipboard";
+          confirm-close-surface = false;
+          window-save-state = "always";
+        }
+        // pkgs.lib.optionalAttrs pkgs.stdenv.hostPlatform.isDarwin {
+          macos-option-as-alt = true;
+          macos-titlebar-style = "hidden";
+          macos-titlebar-proxy-icon = "hidden";
+
+          macos-icon = "custom-style";
+          macos-icon-ghost-color = "white";
+          macos-icon-screen-color = "black";
+        };
       };
     };
-  };
 }
