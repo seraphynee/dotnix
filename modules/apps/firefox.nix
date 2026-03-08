2dc16bfc642865f97f359b5a006531b64412a66e
seraphynee <seraphyne31@gmail.com>
2026-03-08 18:53:19 +0700

    feat: add firefox theme

--- modules/apps/firefox.nix
+++ modules/apps/firefox.nix
@@ -1,6 +1,13 @@
 { __findFile, ... }:
 {
   den.aspects.apps._.firefox.homeManager = {
-    programs.firefox.enable = true;
+    programs.firefox = {
+      enable = true;
+      profiles.default = {
+        settings = {
+          "extensions.activeThemeID" = "firefox-compact-dark@mozilla.org";
+        };
+      };
+    };
   };
 }
