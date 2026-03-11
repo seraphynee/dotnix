fabf93f7b44c7174ebe96b5c965d4b508407c3c0
chianyungcode <cnytechcode@gmail.com>
2026-03-11 15:54:28 +0700

    chore: disable some command to entering the shell

--- devenv.nix
+++ devenv.nix
@@ -23,15 +23,15 @@
   # services.postgres.enable = true;
 
   # https://devenv.sh/scripts/
-  scripts.hello.exec = ''
-    echo hello from $GREET
-  '';
-
-  # https://devenv.sh/basics/
-  enterShell = ''
-    hello         # Run scripts directly
-    git --version # Use packages
-  '';
+  # scripts.hello.exec = ''
+  #   echo hello from $GREET
+  # '';
+  #
+  # # https://devenv.sh/basics/
+  # enterShell = ''
+  #   hello         # Run scripts directly
+  #   git --version # Use packages
+  # '';
 
   # https://devenv.sh/tasks/
   # tasks = {
