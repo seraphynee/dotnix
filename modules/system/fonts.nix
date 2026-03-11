b5623f537c38d3efa9d6241821c013fee6827097
Chian Yung <144510626+chianyungcode@users.noreply.github.com>
2026-03-11 17:18:20 +0700

    font packages (#21)
    * feat: added a configuration to install private fonts from my local private github repo
    
    * feat: use devenv to replacing devShell because it's more faster
    
    * chore: disable some command to entering the shell

--- modules/system/fonts.nix
+++ modules/system/fonts.nix
@@ -1,6 +1,26 @@
+{ lib, ... }:
 {
   den.aspects.system._.fonts.nixos =
     { pkgs, ... }:
+    let
+      privateFontPath = /var/lib/private-fonts;
+      hasPrivateFont = builtins.pathExists privateFontPath;
+
+      privateFont = pkgs.stdenvNoCC.mkDerivation {
+        pname = "private-fonts";
+        version = "unstable";
+        src = privateFontPath;
+        dontConfigure = true;
+        dontBuild = true;
+        installPhase = ''
+          mkdir -p $out/share/fonts/truetype
+          mkdir -p $out/share/fonts/opentype
+          find . -type f -iname '*.ttf' -exec install -m444 -t $out/share/fonts/truetype {} +
+          find . -type f -iname '*.otf' -exec install -m444 -t $out/share/fonts/opentype {} +
+        '';
+      };
+
+    in
     {
       fonts = {
         fontconfig = {
@@ -27,35 +47,43 @@
           };
         };
 
-        packages = with pkgs; [
-          # Nerd Fonts
-          nerd-fonts.fira-code
-          nerd-fonts.atkynson-mono
-          nerd-fonts.caskaydia-cove
-          nerd-fonts.caskaydia-mono
-          nerd-fonts.hack
-          nerd-fonts.iosevka
-          nerd-fonts.jetbrains-mono
+        packages =
+          with pkgs;
+          [
+            # Nerd Fonts
+            nerd-fonts.fira-code
+            nerd-fonts.atkynson-mono
+            nerd-fonts.caskaydia-cove
+            nerd-fonts.caskaydia-mono
+            nerd-fonts.hack
+            nerd-fonts.iosevka
+            nerd-fonts.jetbrains-mono
 
-          # System fonts
-          noto-fonts
-          noto-fonts-cjk-sans
-          noto-fonts-color-emoji
-          liberation_ttf
+            # System fonts
+            noto-fonts
+            noto-fonts-cjk-sans
+            noto-fonts-color-emoji
+            liberation_ttf
 
-          # Programming fonts
-          jetbrains-mono
-          fira-code
-          source-code-pro
+            # Programming fonts
+            jetbrains-mono
+            fira-code
+            source-code-pro
 
-          # Popular fonts
-          roboto
-          open-sans
-          ubuntu-classic
+            # Popular fonts
+            roboto
+            open-sans
+            ubuntu-classic
+            martian-mono
+            _0xproto
+            geist-font
+            monaspace
+            hack-font
 
-          # Icon fonts
-          font-awesome
-        ];
+            # Icon fonts
+            font-awesome
+          ]
+          ++ lib.optional hasPrivateFont privateFont;
       };
     };
 }
