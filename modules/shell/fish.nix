43f26c094ed6099689dd60b6699900e637c8759b
seraphynee <seraphyne31@gmail.com>
2026-03-13 03:25:59 +0700

    feat: disable fish in HM and prefer just installed it

--- modules/shell/fish.nix
+++ modules/shell/fish.nix
@@ -2,15 +2,39 @@
   den.aspects.shell._.fish.homeManager =
     { pkgs, ... }:
     {
+      home.packages = [ pkgs.fish ];
       programs = {
         command-not-found.enable = false;
         nix-index-database.comma.enable = true;
 
         fish = {
-          enable = true;
+          enable = false;
           interactiveShellInit = ''
             abbr --add cl clear
           '';
+          functions = {
+            tmux = ''
+              if test (count $argv) -eq 0
+                  set -l dir (basename "$PWD")
+                  set -l session (string replace -ra '[^A-Za-z0-9_.-]' '_' -- $dir)
+                  test -n "$session"; or set session main
+
+                  if set -q TMUX
+                      if command tmux has-session -t "$session" 2>/dev/null
+                          command tmux switch-client -t "$session"
+                      else
+                          command tmux new-session -d -s "$session" -c "$PWD"
+                          command tmux switch-client -t "$session"
+                      end
+                  else
+                      command tmux new-session -A -s "$session" -c "$PWD"
+                  end
+                  return
+              end
+
+              command tmux $argv
+            '';
+          };
           # interactiveShellInit = ''
           #   ${pkgs.any-nix-shell}/bin/any-nix-shell fish --info-right | source
           # '';
