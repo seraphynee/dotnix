a5ecaf4d6f0049198570ea13ff558c845c735d35
seraphynee <seraphyne31@gmail.com>
2026-03-13 22:45:47 +0700

    feat: replace static username based on den aware context

--- modules/shell/codex.nix
+++ modules/shell/codex.nix
@@ -1,48 +1,50 @@
 {
   __findFile,
   inputs,
-  constants,
   ...
 }:
 {
-  den.aspects.shell._.codex = {
-    homeManager =
-      { pkgs, ... }:
-      let
-        inherit (pkgs.stdenv.hostPlatform) isDarwin isLinux;
+  den.aspects.shell._.codex =
+    { user, ... }:
+    {
+      homeManager =
+        { pkgs, ... }:
+        let
+          inherit (pkgs.stdenv.hostPlatform) isDarwin isLinux;
+          username = user.userName;
 
-        trustedProjects =
-          if isDarwin then
-            ''
-              [projects."/Users/${constants.user_one}/.local/share/chezmoi"]
-              trust_level = "trusted"
+          trustedProjects =
+            if isDarwin then
+              ''
+                [projects."/Users/${username}/.local/share/chezmoi"]
+                trust_level = "trusted"
 
-              [projects."/Users/${constants.user_one}/Code/Personal/Projects/Backend/allweezy-backend"]
-              trust_level = "trusted"
-            ''
-          else if isLinux then
-            ''
-              [projects."/home/${constants.user_two}/dotnix"]
-              trust_level = "trusted"
-            ''
-          else
-            "";
+                [projects."/Users/${username}/Code/Personal/Projects/Backend/allweezy-backend"]
+                trust_level = "trusted"
+              ''
+            else if isLinux then
+              ''
+                [projects."/home/${username}/dotnix"]
+                trust_level = "trusted"
+              ''
+            else
+              "";
 
-        codexConfig = builtins.replaceStrings [ "@conditional_trusted_projects@" ] [ trustedProjects ] (
-          builtins.readFile ../../dots/dot_codex/config.toml.tmpl
-        );
-      in
-      {
-        home.file.".codex/config.toml".text = codexConfig;
-      };
+          codexConfig = builtins.replaceStrings [ "@conditional_trusted_projects@" ] [ trustedProjects ] (
+            builtins.readFile ../../dots/dot_codex/config.toml.tmpl
+          );
+        in
+        {
+          home.file.".codex/config.toml".text = codexConfig;
+        };
 
-    nixos =
-      { pkgs, ... }:
-      let
-        inherit (inputs.nixpkgs-master.legacyPackages.${pkgs.system}) codex;
-      in
-      {
-        environment.systemPackages = [ codex ];
-      };
-  };
+      nixos =
+        { pkgs, ... }:
+        let
+          inherit (inputs.nixpkgs-master.legacyPackages.${pkgs.system}) codex;
+        in
+        {
+          environment.systemPackages = [ codex ];
+        };
+    };
 }
