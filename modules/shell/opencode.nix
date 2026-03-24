58e2b866fc7e63e9dd3e78d3f26052692831703c
seraphynee <seraphyne31@gmail.com>
2026-03-24 19:13:45 +0700

    feat: add opencode config via nix
    close NDD-35

--- modules/shell/opencode.nix
+++ modules/shell/opencode.nix
@@ -4,13 +4,78 @@
   ...
 }:
 {
-  den.aspects.shell._.opencode = _: {
-    homeManager =
-      { config, ... }:
-      {
-        programs.opencode = {
-          enable = true;
+  den.aspects.shell._.opencode =
+    { user, ... }:
+    {
+      nixos =
+        { pkgs, ... }:
+        {
+          environment.systemPackages = with pkgs; [ opencode ];
         };
-      };
-  };
+
+      homeManager =
+        { config, ... }:
+        {
+          sops.templates."opencode-config.json" = {
+            path = "${config.home.homeDirectory}/.config/opencode/opencode.json";
+            mode = "0600";
+            content = builtins.toJSON {
+              "$schema" = "https://opencode.ai/config.json";
+              mcp = {
+                "Ref" = {
+                  type = "remote";
+                  url = "https://api.ref.tools/mcp?apiKey=${config.sops.placeholder."llm/ref_apikey"}";
+                  enabled = true;
+                };
+                deepwiki = {
+                  type = "remote";
+                  url = "https://mcp.deepwiki.com/mcp";
+                  headers = { };
+                  enabled = true;
+                };
+                gitmcp = {
+                  type = "remote";
+                  url = "https://gitmcp.io/chianyungcode/dotfiles";
+                  headers = { };
+                  enabled = false;
+                };
+                exa = {
+                  type = "remote";
+                  url = "https://mcp.exa.ai/mcp";
+                  headers = { };
+                  enabled = true;
+                };
+                context7 = {
+                  type = "remote";
+                  url = "https://mcp.context7.com/mcp";
+                  headers = {
+                    CONTEXT7_API_KEY = config.sops.placeholder."llm/context7_apikey";
+                  };
+                  enabled = true;
+                };
+                grep = {
+                  type = "remote";
+                  url = "https://mcp.grep.app";
+                  enabled = false;
+                };
+                linear = {
+                  type = "local";
+                  command = [
+                    "bun"
+                    "x"
+                    "mcp-remote"
+                    "https://mcp.linear.app/mcp"
+                  ];
+                  enabled = true;
+                };
+                tavily = {
+                  type = "remote";
+                  url = "https://mcp.tavily.com/mcp/?tavilyApiKey=${config.sops.placeholder."llm/tavily_apikey"}";
+                  enabled = true;
+                };
+              };
+            };
+          };
+        };
+    };
 }
