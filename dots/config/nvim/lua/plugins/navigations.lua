997ecc2ab0a7d311a456b657410e3920eab438f1
seraphynee <seraphyne31@gmail.com>
2026-03-24 18:37:31 +0700

    fix(nvim): fix fff.nvim to using nix for machine that have nix

--- dots/config/nvim/lua/plugins/navigations.lua
+++ dots/config/nvim/lua/plugins/navigations.lua
@@ -76,14 +76,8 @@ return {
   -- DESC: File picker, alternative to snacks.picker, telescope, mini.picker and etc.
   {
     "dmtrKovalenko/fff.nvim",
-    enabled = false,
-    build = function()
-      -- this will download prebuild binary or try to use existing rustup toolchain to build from source
-      -- (if you are using lazy you can use gb for rebuilding a plugin if needed)
-      require("fff.download").download_or_build_binary()
-    end,
-    -- if you are using nixos
-    -- build = "nix run .#release",
+    enabled = true,
+    build = "nix run .#release",
     opts = { -- (optional)
       debug = {
         enabled = true, -- we expect your collaboration at least during the beta
