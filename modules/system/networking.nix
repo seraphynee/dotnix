506b35cd52891d60065e45b2f53e4a22a67df4fd
chianyungcode <cnytechcode@gmail.com>
2026-04-13 00:30:58 +0700

    fix: fix networking module

--- modules/system/networking.nix
+++ modules/system/networking.nix
@@ -1,12 +1,13 @@
 {
   den.aspects.system._.networking = {
-    networking = {
-      networkmanager.enable = true;
-      wireless.enable = false;
-      nameservers = [
-        "1.1.1.1" # Cloudflare
-        "8.8.8.8" # Google
-      ];
+    nixos = {
+      networking = {
+        networkmanager.enable = true;
+        nameservers = [
+          "1.1.1.1" # Cloudflare
+          "8.8.8.8" # Google
+        ];
+      };
     };
   };
 }
