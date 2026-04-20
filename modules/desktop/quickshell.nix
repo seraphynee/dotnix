8b99c63261612fc678ac5d5a9bc4a87abc68e445
seraphynee <seraphyne31@gmail.com>
2026-04-20 15:32:19 +0700

    feat(noctalia): delete boot to uefi in session menu option

--- modules/desktop/quickshell.nix
+++ modules/desktop/quickshell.nix
@@ -76,6 +76,47 @@
               settings = {
                 # configure noctalia here
                 settingsVersion = 57;
+                sessionMenu = {
+                  enableCountdown = true;
+                  countdownDuration = 10000;
+                  position = "center";
+                  showHeader = true;
+                  showKeybinds = true;
+                  largeButtonsStyle = true;
+                  largeButtonsLayout = "single-row";
+                  powerOptions = [
+                    {
+                      action = "lock";
+                      enabled = true;
+                      keybind = "1";
+                    }
+                    {
+                      action = "suspend";
+                      enabled = true;
+                      keybind = "2";
+                    }
+                    {
+                      action = "hibernate";
+                      enabled = true;
+                      keybind = "3";
+                    }
+                    {
+                      action = "reboot";
+                      enabled = true;
+                      keybind = "4";
+                    }
+                    {
+                      action = "logout";
+                      enabled = true;
+                      keybind = "5";
+                    }
+                    {
+                      action = "shutdown";
+                      enabled = true;
+                      keybind = "6";
+                    }
+                  ];
+                };
                 bar = {
                   barType = "framed";
                   density = "default";
