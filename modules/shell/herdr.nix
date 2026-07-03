4352a1330278f081be31aa931e4071174c907ab7
seraphynee <seraphyne31@gmail.com>
2026-07-03 16:27:28 +0700

    feat: add herdr completions and configure sidebar to hidden completely
    close NDD-71

--- modules/shell/herdr.nix
+++ modules/shell/herdr.nix
@@ -1,13 +1,25 @@
 { __findFile, inputs, ... }: {
   den.aspects.shell._.herdr = {
-    homeManager = _: {
-      xdg.configFile."herdr/config.toml".source = ../../dots/config/herdr/config.toml;
-    };
+    homeManager =
+      { pkgs, ... }:
+      let
+        herdr = inputs.herdr.packages.${pkgs.stdenv.hostPlatform.system}.herdr;
+        herdrFishCompletion = pkgs.runCommand "herdr-fish-completion" { } ''
+          ${herdr}/bin/herdr completion fish > $out
+        '';
+      in
+      {
+        xdg.configFile."herdr/config.toml".source = ../../dots/config/herdr/config.toml;
+        xdg.configFile."fish/completions/herdr.fish".source = herdrFishCompletion;
+      };
 
     nixos =
       { pkgs, ... }:
+      let
+        herdr = inputs.herdr.packages.${pkgs.stdenv.hostPlatform.system}.herdr;
+      in
       {
-        environment.systemPackages = [ inputs.herdr.packages.${pkgs.stdenv.hostPlatform.system}.herdr ];
+        environment.systemPackages = [ herdr ];
       };
   };
 }
