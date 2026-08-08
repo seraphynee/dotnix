388709cfe891aec93eb5de20c8a7be681ee9c6e7
seraphynee <seraphyne31@gmail.com>
2026-08-08 22:28:19 +0700

    feat(zed): generate secret-aware settings

--- modules/apps/zed.nix
+++ modules/apps/zed.nix
@@ -2,13 +2,37 @@
 {
   den.aspects.apps._.zed = {
     homeManager =
-      { pkgs, ... }:
+      { config, pkgs, ... }:
+      let
+        settings =
+          builtins.replaceStrings
+            [ "    \"nil\": {" ]
+            [
+              (
+                ''
+                      "wakatime": {
+                        "initialization_options": {
+                          "api-key": "${config.sops.placeholder."productivity/wakatime_apikey"}"
+                        }
+                      },
+                  ''
+                + ''    "nil": {''
+              )
+            ]
+            (builtins.readFile ../../dots/config/zed/settings.json);
+      in
       {
         home.packages = [ pkgs.zed-editor ];
 
-        xdg.configFile."zed" = {
-          source = ../../dots/config/zed;
-          recursive = true;
+        xdg.configFile = {
+          "zed/keymap.json".source = ../../dots/config/zed/keymap.json;
+          "zed/tasks.json".source = ../../dots/config/zed/tasks.json;
+        };
+
+        sops.templates."zed-settings.json" = {
+          path = "${config.xdg.configHome}/zed/settings.json";
+          mode = "0600";
+          content = settings;
         };
       };
   };
