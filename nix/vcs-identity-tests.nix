4d6204bd1575c60060827796df1441896212e92f
seraphynee <seraphyne31@gmail.com>
2026-08-14 02:37:53 +0700

    refactor(vcs): drive git from the shared identity profile

--- nix/vcs-identity-tests.nix
+++ nix/vcs-identity-tests.nix
@@ -1,4 +1,5 @@
 {
+  inputs,
   lib,
   ...
 }:
@@ -26,6 +27,24 @@
           ];
         };
 
+      mkVcsHome =
+        profile:
+        inputs.home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [
+            ../lib/shell/vcs/profile.nix
+            ../lib/shell/vcs/git.nix
+            {
+              home = {
+                username = "vcs-test";
+                homeDirectory = if pkgs.stdenv.isDarwin then "/Users/vcs-test" else "/home/vcs-test";
+                stateVersion = "26.11";
+              };
+              dotnix.vcs = profile;
+            }
+          ];
+        };
+
       assertionsPass = home: lib.all (entry: entry.assertion) home.config.assertions;
 
       valid = mkProfileHome {
@@ -45,6 +64,24 @@
         identity.name = "missing-email";
         jujutsu.enable = true;
       };
+
+      seraphyne = mkVcsHome {
+        identity = {
+          name = "seraphynee";
+          email = "seraphyne31@gmail.com";
+        };
+        github.username = "seraphynee";
+        git.enable = true;
+      };
+
+      chianyung = mkVcsHome {
+        identity = {
+          name = "chianyungcode";
+          email = "cnytechcode@gmail.com";
+        };
+        github.username = "chianyungcode";
+        git.enable = true;
+      };
     in
     {
       checks.vcs-identity =
@@ -53,6 +90,22 @@
         assert assertionsPass valid;
         assert !assertionsPass missingName;
         assert !assertionsPass missingEmail;
+        assert assertionsPass seraphyne;
+        assert assertionsPass chianyung;
+        assert
+          seraphyne.config.programs.git.settings.user == {
+            name = "seraphynee";
+            email = "seraphyne31@gmail.com";
+          };
+        assert
+          chianyung.config.programs.git.settings.user == {
+            name = "chianyungcode";
+            email = "cnytechcode@gmail.com";
+          };
+        assert seraphyne.config.programs.git.settings.user != chianyung.config.programs.git.settings.user;
+        assert
+          seraphyne.config.programs.git.settings.credential."https://github.com".username == "seraphynee";
+        assert seraphyne.config.programs.git.settings.alias.co == "checkout";
         pkgs.runCommand "vcs-identity-evaluation" { } ''
           touch "$out"
         '';
