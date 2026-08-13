ed2241d4c690a5793bcf4d0f1868598b7dfe59c7
seraphynee <seraphyne31@gmail.com>
2026-08-13 22:34:48 +0700

    fix(herdr): bootstrap plugins per session

--- modules/shell/herdr.nix
+++ modules/shell/herdr.nix
@@ -1,28 +1,34 @@
 { __findFile, inputs, ... }: {
   den.aspects.shell._.herdr = {
     homeManager =
-      { lib, pkgs, ... }:
+      { pkgs, ... }:
       let
         herdr = inputs.herdr.packages.${pkgs.stdenv.hostPlatform.system}.herdr;
         herdrAutomaticRename = inputs.herdr-automatic-rename;
         herdrFishCompletion = pkgs.runCommand "herdr-fish-completion" { } ''
           ${herdr}/bin/herdr completion fish > $out
         '';
+        herdrPluginBootstrap = pkgs.writeShellApplication {
+          name = "herdr-plugin-bootstrap";
+          runtimeInputs = [
+            herdr
+            pkgs.jq
+          ];
+          text = builtins.readFile ./herdr-plugin-bootstrap.sh;
+        };
       in
       {
-        home.activation.herdrAutomaticRename = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-          run ${herdr}/bin/herdr plugin link ${herdrAutomaticRename}
-        '';
-
         xdg.configFile."herdr/config.toml".source = ../../dots/config/herdr/config.toml;
         xdg.configFile."fish/completions/herdr.fish".source = herdrFishCompletion;
         xdg.configFile."fish/conf.d/herdr.fish".text = ''
           if type -q herdr
+              ${herdrPluginBootstrap}/bin/herdr-plugin-bootstrap ${herdrAutomaticRename}
               source ${herdrAutomaticRename}/shell/hook.fish
           end
         '';
         xdg.configFile."zsh/conf.d/third-party/herdr-automatic-rename-nix.sh".text = ''
           if (( $+commands[herdr] )); then
+            ${herdrPluginBootstrap}/bin/herdr-plugin-bootstrap ${herdrAutomaticRename}
             source "${herdrAutomaticRename}/shell/hook.zsh"
           fi
         '';
