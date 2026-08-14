87ad60baf5bcc03a3ddfd124c938bf290de3ae6e
seraphynee <seraphyne31@gmail.com>
2026-08-14 23:11:16 +0700

    feat: replacing direnv with native devenv auto activation

--- modules/shell/packages.nix
+++ modules/shell/packages.nix
@@ -199,6 +199,11 @@
           enableNushellIntegration = true;
         };
 
+        devenv = {
+          enable = true;
+          enableFishIntegration = true;
+        };
+
         direnv = {
           enable = true;
           nix-direnv.enable = true;
