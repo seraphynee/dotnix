14fadcda9c67a9e7d334ee83660fe05c6f22b0f0
chianyungcode <cnytechcode@gmail.com>
2026-08-17 05:54:19 +0700

    feat: disable harper_ls LSP in nvim-lspconfig and prevent mason installing lua_ls LSP

--- dots/config/nvim/lua/plugins/lsp.lua
+++ dots/config/nvim/lua/plugins/lsp.lua
@@ -18,7 +18,7 @@ return {
         "gopls", -- Go
         "html", -- HTML
         "jsonls", -- JSON
-        "lua_ls", -- Lua
+        -- "lua_ls", -- Lua
         "markdown_oxide", -- Markdown
         "prismals", -- Prisma
         "ruff", -- Python
@@ -67,6 +67,9 @@ return {
           },
         },
         nil_ls = {
+          enabled = false,
+        },
+        harper_ls = {
           enabled = false,
         },
         nixd = {
