0347aa50095adc028d6d6c74432b715d4763ee56
seraphynee <seraphyne31@gmail.com>
2026-08-24 14:15:18 +0700

    feat(nvim): disable spellcheck in markdown file

--- dots/config/nvim/init.lua
+++ dots/config/nvim/init.lua
@@ -1,2 +1,10 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- disable spell checking in markdown files
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.opt_local.spell = false
+  end,
+})
