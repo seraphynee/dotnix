bff59f4272c65f7004f3cd81ec8f934a50ed6434
seraphynee <seraphyne31@gmail.com>
2026-08-27 12:26:26 +0700

    abbreviations: update git abbreviations in fish shell

--- dots/config/fish/conf.d/abbreviations.fish
+++ dots/config/fish/conf.d/abbreviations.fish
@@ -276,29 +276,24 @@ abbr pueon "pueued -d"
 
 # git
 abbr --add g git
+
 abbr --add ga "git add"
 abbr --add gaa "git add --all"
+abbr --add gapa "git add --patch"
+abbr --add gau "git add --update"
+abbr --add gav "git add --verbose"
+
 abbr --add gam "git am"
 abbr --add gama "git am --abort"
 abbr --add gamc "git am --continue"
 abbr --add gams "git am --skip"
 abbr --add gamscp "git am --show-current-patch"
+
 abbr --add gap "git apply"
-abbr --add gapa "git add --patch"
 abbr --add gapt "git apply --3way"
-abbr --add gau "git add --update"
-abbr --add gav "git add --verbose"
-abbr --add gb "git branch"
-abbr --add gba "git branch --all"
-abbr --add gbD "git branch --delete --force"
-abbr --add gbd "git branch --delete"
-# abbr "gbg"="LANG=C git branch -vv | grep \": gone\]\""
-# abbr "gbgd"="LANG=C git branch --no-color -vv | grep \": gone\]\" | cut -c 3- | awk '{print \$1}' | xargs git branch -d"
-# abbr "gbgD"="LANG=C git branch --no-color -vv | grep \": gone\]\" | cut -c 3- | awk '{print \$1}' | xargs git branch -D"
+
 abbr --add gbl "git blame -w"
-abbr --add gbm "git branch --move"
-abbr --add gbnm "git branch --no-merged"
-abbr --add gbr "git branch --remote"
+
 abbr --add gbs "git bisect"
 abbr --add gbsb "git bisect bad"
 abbr --add gbsg "git bisect good"
@@ -306,6 +301,33 @@ abbr --add gbsn "git bisect new"
 abbr --add gbso "git bisect old"
 abbr --add gbsr "git bisect reset"
 abbr --add gbss "git bisect start"
+
+abbr --add gb "git branch"
+abbr --add gba "git branch --all"
+abbr --add gbD "git branch --delete --force"
+abbr --add gbd "git branch --delete"
+abbr --add gbm "git branch --move"
+abbr --add gbnm "git branch --no-merged"
+abbr --add gbr "git branch --remote"
+abbr --add ggsup "git branch --set-upstream-to=origin/\$(git_current_branch)"
+
+abbr --add gcB "git checkout -B"
+abbr --add gcb "git checkout -b"
+abbr --add gcd "git checkout \$(git_develop_branch)"
+abbr --add gcm "git checkout \$(git_main_branch)"
+abbr --add gco "git checkout"
+abbr --add gcor "git checkout --recurse-submodules"
+
+abbr --add gcp "git cherry-pick"
+abbr --add gcpa "git cherry-pick --abort"
+abbr --add gcpc "git cherry-pick --continue"
+
+abbr --add gclean "git clean --interactive -d"
+
+abbr --add gcl "git clone"
+abbr --add gclr "git clone --recurse-submodules"
+abbr --add gclf "git clone --recursive --shallow-submodules --filter=blob:none --also-filter-submodules"
+
 abbr --add gc "git commit --verbose"
 abbr --add gc\! "git commit --verbose --amend"
 abbr --add gca "git commit --verbose --all"
@@ -316,54 +338,37 @@ abbr --add gcann\! 'git commit --verbose --all --date=now --no-edit --amend'
 abbr --add gcans\! 'git commit --verbose --all --signoff --no-edit --amend'
 abbr --add gcas "git commit --all --signoff"
 abbr --add gcasm "git commit --all --signoff --message"
-abbr --add gcB "git checkout -B"
-abbr --add gcb "git checkout -b"
-abbr --add gcd "git checkout \$(git_develop_branch)"
-abbr --add gcf "git config --list"
 abbr --add gcfu "git commit --fixup"
-abbr --add gcl "git clone"
-abbr --add gclr "git clone --recurse-submodules"
-abbr --add gclean "git clean --interactive -d"
-abbr --add gclf "git clone --recursive --shallow-submodules --filter=blob:none --also-filter-submodules"
-abbr --add gcm "git checkout \$(git_main_branch)"
-abbr --add gcmsg "git commit --message"
+abbr --add gcmsg --set-cursor 'git commit --message "%"'
 abbr --add gcn "git commit --verbose --no-edit"
-# abbr --add gcn\! "git commit --verbose --no-edit --amend"
-abbr --add gco "git checkout"
-abbr --add gcor "git checkout --recurse-submodules"
-abbr --add gcount "git shortlog --summary --numbered"
-abbr --add gcp "git cherry-pick"
-abbr --add gcp1 "ssh gcp1"
-abbr --add gcpa "git cherry-pick --abort"
-abbr --add gcpc "git cherry-pick --continue"
 abbr --add gcs "git commit --gpg-sign"
 abbr --add gcsm "git commit --signoff --message"
 abbr --add gcss "git commit --gpg-sign --signoff"
 abbr --add gcssm "git commit --gpg-sign --signoff --message"
+
+abbr --add gcf "git config --list"
+
+abbr --add gdct "git describe --tags \$(git rev-list --tags --max-count=1)"
+
 abbr --add gd "git diff"
 abbr --add gdca "git diff --cached"
-abbr --add gdct "git describe --tags \$(git rev-list --tags --max-count=1)"
 abbr --add gdcw "git diff --cached --word-diff"
 abbr --add gds "git diff --staged"
-abbr --add gdt "git diff-tree --no-commit-id --name-only -r"
 abbr --add gdup "git diff @{upstream}"
 abbr --add gdw "git diff --word-diff"
+
+abbr --add gdt "git diff-tree --no-commit-id --name-only -r"
+
 abbr --add gf "git fetch"
 abbr --add gfa "git fetch --all --tags --prune --jobs=10"
-abbr --add gfg "git ls-files | grep"
 abbr --add gfo "git fetch origin"
+
 abbr --add gg "git gui citool"
 abbr --add gga "git gui citool --amend"
-# abbr "ggpull"="git pull origin \"\$(git_current_branch)\""
-abbr --add ggpur ggu
-# abbr "ggpush"="git push origin \"\$(git_current_branch)\""
-abbr --add ggsup "git branch --set-upstream-to=origin/\$(git_current_branch)"
-abbr --add gignore "git update-index --assume-unchanged"
-# abbr "gignored"="git ls-files -v | grep \"^[[:lower:]]\""
-abbr --add git-svn-dcommit-push "git svn dcommit && git push github \$(git_main_branch):svntrunk"
+
 abbr --add gk "\gitk --all --branches &\!"
 abbr --add gke "\gitk --all \$(git log --walk-reflogs --pretty=%h) &\!"
-abbr --add gl "git pull"
+
 abbr --add glg "git log --stat"
 abbr --add glgg "git log --graph"
 abbr --add glgga "git log --graph --decorate --branches --remotes --tags"
@@ -380,39 +385,44 @@ abbr --add glol 'git log --graph --pretty="%Cred%h%Creset -%C(auto)%d%Creset %s 
 abbr --add glolaa 'git log --graph --pretty="%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset" --all'
 abbr --add glola 'git log --graph --pretty="%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset" --branches --remotes --tags'
 abbr --add glols 'git log --graph --pretty="%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset" --stat'
+
 abbr --add glp 'git log-patch'
-abbr --add gluc "git pull upstream \$(git_current_branch)"
-abbr --add glum "git pull upstream \$(git_main_branch)"
+
+abbr --add gfg "git ls-files | grep"
+
 abbr --add gm "git merge"
 abbr --add gma "git merge --abort"
 abbr --add gmc "git merge --continue"
 abbr --add gmff "git merge --ff-only"
 abbr --add gmom "git merge origin/\$(git_main_branch)"
 abbr --add gms "git merge --squash"
+abbr --add gmum "git merge upstream/\$(git_main_branch)"
+
 abbr --add gmtl "git mergetool --no-prompt"
 abbr --add gmtlvim "git mergetool --no-prompt --tool=vimdiff"
-abbr --add gmum "git merge upstream/\$(git_main_branch)"
-abbr --add gp "git push"
-abbr --add gpd "git push --dry-run"
-abbr --add gpf "git push --force-with-lease --force-if-includes"
-# abbr --add gpf\! "git push --force"
-abbr --add gpoat "git push origin --all && git push origin --tags"
-abbr --add gpod "git push origin --delete"
+
+abbr --add ggpur ggu
+abbr --add gl "git pull"
+abbr --add gluc "git pull upstream \$(git_current_branch)"
+abbr --add glum "git pull upstream \$(git_main_branch)"
 abbr --add gpr "git pull --rebase"
 abbr --add gpra "git pull --rebase --autostash"
 abbr --add gprav "git pull --rebase --autostash -v"
-abbr --add gpristine "git reset --hard && git clean --force -dfx"
 abbr --add gprom "git pull --rebase origin \$(git_main_branch)"
 abbr --add gpromi "git pull --rebase=interactive origin \$(git_main_branch)"
 abbr --add gprum "git pull --rebase upstream \$(git_main_branch)"
 abbr --add gprumi "git pull --rebase=interactive upstream \$(git_main_branch)"
 abbr --add gprv "git pull --rebase -v"
+abbr --add gp "git push"
+abbr --add gpd "git push --dry-run"
+abbr --add gpf "git push --force-with-lease --force-if-includes"
+abbr --add gpoat "git push origin --all && git push origin --tags"
+abbr --add gpod "git push origin --delete"
 abbr --add gpsup "git push --set-upstream origin \$(git_current_branch)"
 abbr --add gpsupf "git push --set-upstream origin \$(git_current_branch) --force-with-lease --force-if-includes"
 abbr --add gpu "git push upstream"
 abbr --add gpv "git push --verbose"
-abbr --add gr "git remote"
-abbr --add gra "git remote add"
+
 abbr --add grb "git rebase"
 abbr --add grba "git rebase --abort"
 abbr --add grbc "git rebase --continue"
@@ -423,35 +433,43 @@ abbr --add grbo "git rebase --onto"
 abbr --add grbom "git rebase origin/\$(git_main_branch)"
 abbr --add grbs "git rebase --skip"
 abbr --add grbum "git rebase upstream/\$(git_main_branch)"
-abbr --add grev "git revert"
-abbr --add greva "git revert --abort"
-abbr --add grevc "git revert --continue"
+
 abbr --add grf "git reflog"
+
+abbr --add gr "git remote"
+abbr --add gra "git remote add"
+abbr --add grmv "git remote rename"
+abbr --add grrm "git remote remove"
+abbr --add grset "git remote set-url"
+abbr --add grup "git remote update"
+abbr --add grv "git remote --verbose"
+
+abbr --add gpristine "git reset --hard && git clean --force -dfx"
 abbr --add grh "git reset"
 abbr --add grhh "git reset --hard"
 abbr --add grhk "git reset --keep"
 abbr --add grhs "git reset --soft"
-abbr --add grm "git rm"
-abbr --add grmc "git rm --cached"
-abbr --add grmv "git remote rename"
 abbr --add groh "git reset origin/\$(git_current_branch) --hard"
-abbr --add grrm "git remote remove"
+abbr --add gru "git reset --"
+abbr --add gwipe "git reset --hard && git clean --force -df"
+
 abbr --add grs "git restore"
-abbr --add grset "git remote set-url"
 abbr --add grss "git restore --source"
 abbr --add grst "git restore --staged"
-# abbr "grt"="cd \"\$(git rev-parse --show-toplevel || echo .)\""
-abbr --add gru "git reset --"
-abbr --add grup "git remote update"
-abbr --add grv "git remote --verbose"
-abbr --add gsb "git status --short --branch"
-abbr --add gsd "git svn dcommit"
-abbr --add gsh "git sh"
-abbr --add gsi "git submodule init"
+
+abbr --add grev "git revert"
+abbr --add greva "git revert --abort"
+abbr --add grevc "git revert --continue"
+
+abbr --add grm "git rm"
+abbr --add grmc "git rm --cached"
+
 abbr --add gsps "git show --pretty=short --show-signature"
-abbr --add gsr "git svn rebase"
-abbr --add gss "git status --short"
-abbr --add gst "git status"
+
+abbr --add gcount "git shortlog --summary --numbered"
+
+abbr --add gsh "git sh"
+
 abbr --add gsta "git stash push"
 abbr --add gstaa "git stash apply"
 abbr --add gstall "git stash --all"
@@ -461,43 +479,115 @@ abbr --add gstl "git stash list"
 abbr --add gstp "git stash pop"
 abbr --add gsts "git stash show --patch"
 abbr --add gstu "gsta --include-untracked"
+
+abbr --add gsb "git status --short --branch"
+abbr --add gss "git status --short"
+abbr --add gst "git status"
+
+abbr --add gsi "git submodule init"
 abbr --add gsu "git submodule update"
+
+abbr --add git-svn-dcommit-push "git svn dcommit && git push github \$(git_main_branch):svntrunk"
+abbr --add gsd "git svn dcommit"
+abbr --add gsr "git svn rebase"
+
 abbr --add gsw "git switch"
 abbr --add gswc "git switch --create"
 abbr --add gswd "git switch develop"
 abbr --add gswm "git switch main"
+
 abbr --add gta "git tag --annotate"
-# abbr "gtl"="gtl(){ git tag --sort=-v:refname -n --list \"\${1}*\" }; noglob gtl"
 abbr --add gts "git tag --sign"
 abbr --add gtv "git tag | sort -V"
+
+abbr --add gignore "git update-index --assume-unchanged"
 abbr --add gunignore "git update-index --no-assume-unchanged"
-# abbr "gunwip"="git rev-list --max-count=1 --format=\"%s\" HEAD | grep -q \"\--wip--\" && git reset HEAD~1"
-# abbr "gup"="
-# print -Pu2 \"%F{yellow}[oh-my-zsh] '%F{red}gup%F{yellow}' is a deprecated alias, using '%F{green}gpr%F{yellow}' instead.%f\"
-# gpr"
-# abbr "gupa"="
-# print -Pu2 \"%F{yellow}[oh-my-zsh] '%F{red}gupa%F{yellow}' is a deprecated alias, using '%F{green}gpra%F{yellow}' instead.%f\"
-# gpra"
-# abbr "gupav"="
-# print -Pu2 \"%F{yellow}[oh-my-zsh] '%F{red}gupav%F{yellow}' is a deprecated alias, using '%F{green}gprav%F{yellow}' instead.%f\"
-# gprav"
-# abbr "gupom"="
-# print -Pu2 \"%F{yellow}[oh-my-zsh] '%F{red}gupom%F{yellow}' is a deprecated alias, using '%F{green}gprom%F{yellow}' instead.%f\"
-# gprom"
-# abbr "gupomi"="
-# print -Pu2 \"%F{yellow}[oh-my-zsh] '%F{red}gupomi%F{yellow}' is a deprecated alias, using '%F{green}gpromi%F{yellow}' instead.%f\"
-# gpromi"
-# abbr "gupv"="
-# print -Pu2 \"%F{yellow}[oh-my-zsh] '%F{red}gupv%F{yellow}' is a deprecated alias, using '%F{green}gprv%F{yellow}' instead.%f\"
-# gprv"
+
 abbr --add gwch "git whatchanged -p --abbrev-commit --pretty=medium"
-# abbr "gwip"="git add -A; git rm \$(git ls-files --deleted) 2> /dev/null; git commit --no-verify --no-gpg-sign --message \"--wip-- [skip ci]\""
-abbr --add gwipe "git reset --hard && git clean --force -df"
+
 abbr --add gwt "git worktree"
 abbr --add gwta "git worktree add"
 abbr --add gwtls "git worktree list"
 abbr --add gwtmv "git worktree move"
 abbr --add gwtrm "git worktree remove"
+
+# Jujutsu
+abbr -a j jj # I use `jj` to exit insert mode
+abbr -a jh 'jj -h'
+abbr -a ji jjui
+
+abbr -a jst 'jj status'
+abbr -a jab --set-cursor 'jj abandon %'
+abbr -a jabso 'jj absorb'
+abbr -a je --set-cursor 'jj edit %'
+
+abbr -a jsh --set-cursor 'jj show -r "@%"'
+abbr -a jshs --set-cursor 'jj show -s -r "@%"'
+abbr -a jshst --set-cursor 'jj show -r "@%" --stat'
+
+abbr -a jbl 'jj bookmark list -a'
+abbr -a jbm --set-cursor 'jj bookmark move % --to @-'
+abbr -a jbmm 'jj bookmark move main --to @-'
+abbr -a jbsc 'jj bookmark set -r @'
+
+abbr -a jdf --set-cursor 'jj diff -r "@%"'
+abbr -a jdfs --set-cursor 'jj diff -s -r "@%"'
+
+abbr -a jgf 'jj git fetch'
+abbr -a jgpa 'jj git push'
+abbr -a jgps --set-cursor 'jj git push -b %'
+abbr -a jgpsm --set-cursor 'jj git push -b main'
+
+abbr -a jl 'jj log'
+abbr -a jla "jj log 'all()'"
+abbr -a jlr --set-cursor "jj log -r '%'"
+abbr -a jlt --set-cursor "jj log -T %"
+abbr -a jls --set-cursor 'jj log -s'
+abbr -a jlsr --set-cursor 'jj log -s -r "@%"'
+abbr -a jlsp --set-cursor 'jj log -s -p'
+abbr -a jlspr --set-cursor 'jj log -s -p -r "@%"'
+abbr -a jlp --set-cursor 'jj log -p'
+abbr -a jlps --set-cursor 'jj log -p -s'
+abbr -a jlpsr --set-cursor 'jj log -p -s -r "@%"'
+
+abbr -a jrs --set-cursor 'jj restore %'
+abbr -a jrsi 'jj restore -i'
+
+abbr -a jrb --set-cursor 'jj rebase %'
+abbr -a jrbh --set-cursor 'jj rebase -h'
+abbr -a jrbs --set-cursor 'jj rebase -s % -o @-'
+abbr -a jrbr --set-cursor 'jj rebase -r % -o %'
+
+abbr -a jsp --set-cursor 'jj split -r "@%"'
+abbr -a jspi --set-cursor 'jj split -i -r "@%"'
+abbr -a jspa --set-cursor 'jj split -A %'
+abbr -a jspb --set-cursor 'jj split -B %'
+
+abbr -a jsq --set-cursor 'jj squash -r "@%"'
+abbr -a jsqi --set-cursor 'jj squash -i -r "@%"'
+abbr -a jsqft --set-cursor 'jj squash -f "@%" -t "%"'
+abbr -a jsqift --set-cursor 'jj squash -i -f "@%" -t "%"'
+
+abbr -a jd --set-cursor 'jj desc -r "@%"'
+abbr -a jdc --set-cursor 'jj desc -m "$(koji --stdout)" -r "@%"'
+
+abbr -a jc 'jj commit'
+abbr -a jcc 'jj commit -m "$(koji --stdout)"'
+
+abbr -a jn --set-cursor 'jj new %'
+abbr -a jna --set-cursor 'jj new -A %'
+abbr -a jnb --set-cursor 'jj new -B %'
+abbr -a jnc 'jj new -m "$(koji --stdout)"'
+
+abbr -a judo 'jj undo'
+abbr -a jop --set-cursor 'jj op %'
+abbr -a jopl --set-cursor 'jj op log %'
+abbr -a jopd --set-cursor 'jj op diff %'
+abbr -a jopa --set-cursor 'jj op abandon %'
+abbr -a joprs --set-cursor 'jj op restore %'
+abbr -a joprv --set-cursor 'jj op revert %'
+abbr -a jevl --set-cursor 'jj evolog -r "@%"'
+abbr -a jevlp --set-cursor 'jj evolog -p -r "@%"'
 
 # hunk
 abbr -a hd --set-cursor 'hunk diff "@%"'
