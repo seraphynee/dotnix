03da307448f0d5f53af15bce46a7eb311abaff8e
seraphynee <seraphyne31@gmail.com>
2026-09-03 16:32:03 +0700

    feat(herdr): source all config file for herdr plugins

--- modules/shell/terminal-multiplexer.nix
+++ modules/shell/terminal-multiplexer.nix
@@ -81,12 +81,10 @@
         '';
 
         xdg.configFile."herdr/config.toml".source = ../../dots/config/herdr/config.toml;
-        xdg.configFile."herdr/plugins/config/worktrunk/config.toml".text = ''
-          open_mode        = "workspace"
-          picker_placement = "popup"
-          popup_height     = 24
-          popup_width      = "70%"
-        '';
+        xdg.configFile."herdr/plugins/config" = {
+          source = ../../dots/config/herdr/plugins/config;
+          recursive = true;
+        };
         xdg.configFile."fish/completions/herdr.fish".source = herdrFishCompletion;
         xdg.configFile."fish/conf.d/herdr.fish".text = ''
           if type -q herdr; and test -r ${lib.escapeShellArg automaticRenameFishHookPath}
